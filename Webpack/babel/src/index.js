b4843c6fc33f3f22ba6f52758027791b1147e67d
Steven G <41505038+EstenGrove@users.noreply.github.com>
2019-07-14 20:05:48 -0700

    Create index.js

---
+++ Webpack/babel/src/index.js
@@ -0,0 +1,12 @@
+console.log("%cTemplate building...", "color: #5c75ea; font-size: 12px;");
+console.log(
+  "%cStarter Template now running...",
+  "color: #00C9A7; font-size: 20px;"
+);
+
+// test code - you can remove this
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", e => {
+  alert("Button was clicked!");
+});

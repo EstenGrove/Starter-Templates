2faca808179666930c7e65aa094dcf835866fe59
Steven G <41505038+EstenGrove@users.noreply.github.com>
2019-07-28 12:31:16 -0700

    Create App.js

---
+++ Sass-CSS-Modules-React/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-Header">
+        <h1 className="App-Header_title">Sass CSS-Modules</h1>
+        <button className="btn marginTop-MD charcoal">Learn More</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
